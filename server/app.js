443d79884854649706d33a92b345d48df861a90f
Elbert Lai <elbert@elberts-macbook.lan>
2020-04-21 15:56:54 -0400

    Updated gitignore

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
 // server goes here
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
