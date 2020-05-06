16b0c1f0d7dccce30693047938ff0c4e9f26ef97
jli0423 <justin.li0423@gmail.com>
2020-05-06 19:17:56 -0400

    Updates

---
+++ precache-manifest.9188551783bb83d87d6db847a45b8a53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56050903aae4ab3d349dda44a11caa67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d51a812d86c8299807a",
+    "url": "/static/css/main.706e7acd.chunk.css"
+  },
+  {
+    "revision": "d8c728fb4556b6088cd8",
+    "url": "/static/js/2.c0e0df8f.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.c0e0df8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d51a812d86c8299807a",
+    "url": "/static/js/main.216a9797.chunk.js"
+  },
+  {
+    "revision": "c82b89db6992acda94b6",
+    "url": "/static/js/runtime-main.eec3f691.js"
+  },
+  {
+    "revision": "e6e625d40235f4bf1113381c31c4eee1",
+    "url": "/static/media/tractor_logo.e6e625d4.svg"
+  }
+]);
